32da2ec0b731db9eb1efec795862ce1a38a3005b
Will Park <dpark032@ucr.edu>
2021-04-20 09:07:14 -0700

    Finished UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,10 @@
 import Button from './Button'
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log("click");
-    }
-    
+const Header = ({ title, onAdd, showAddTask }) => {
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick} />
+            <Button color={showAddTask ? 'red' : 'green'} text={showAddTask ? 'Close' : 'Add'} onClick={onAdd} />
         </header>
     )
 }
